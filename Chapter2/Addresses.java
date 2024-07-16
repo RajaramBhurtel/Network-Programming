910300739b5f9842f44bb8e622ab8e058e10b855
RajaramBhurtel <sabinbhurtel@gmail.com>
2024-07-16 08:50:10 +0545

    adding log file

--- Chapter2/Addresses.java
+++ Chapter2/Addresses.java
@@ -4,6 +4,11 @@ public class Addresses {
     public static void main(String[] args) throws Exception {
         InetAddress ip = InetAddress.getLocalHost();
         byte[] bytes = ip.getAddress();
+         for (byte octet : bytes) {
+                int unsignedByte = octet < 0 ? octet + 256 : octet;
+
+              System.out.println(unsignedByte);
+            }
         if (bytes.length == 4) {
             System.out.println("IPv4");
         } else if (bytes.length == 16) {
